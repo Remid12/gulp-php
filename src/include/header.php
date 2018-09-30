48cfe5a43a337de5ff115d7d8e8ee96410894a36
Remi Denimal <rdenimal@conceptfactory.lu>
2018-09-30 12:40:56 +0200

    système de symbols svg + icônes de base (social / search / arrow-down)

--- src/include/header.php
+++ src/include/header.php
@@ -1,3 +1,5 @@
+<?php include('./include/symbols.php'); ?>
+
 <header class="header">
     <div class="inner">
        <img src="assets/images/logo-nasa.svg" alt="logo" class="logo">
@@ -7,6 +9,7 @@
 				<li class="menu__item"><a href="">Home</a></li>
 				<li class="menu__item"><a href="">About</a></li>
 				<li class="menu__item"><a href="">Contact</a></li>
+				<li class="menu__item"><a href=""><svg class="icon" viewBox="0 0 512 512"><use xlink:href="#icon-search"/></svg></a></li>
 				<li class="menu__item"><button class="btn btn-cta">Subscribe</button></li>
 				
 			</ul>
